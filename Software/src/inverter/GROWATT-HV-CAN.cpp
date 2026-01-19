178e4146f5e772b9ab817e25860e950c64898ec3
ErikssonPer <127212101+ErikssonPer@users.noreply.github.com>
2026-01-19 22:01:51 +0100

    Refactor capacity limit checks for readability

--- Software/src/inverter/GROWATT-HV-CAN.cpp
+++ Software/src/inverter/GROWATT-HV-CAN.cpp
@@ -55,8 +55,12 @@ void GrowattHvInverter::
       const uint32_t soc_pct = (uint32_t)(datalayer.battery.status.reported_soc / 100);  // 0..100
       rem_10mAh = (uint32_t)((uint64_t)full_10mAh * soc_pct / 100ULL);
     }
-    if (full_10mAh > 0xFFFF) full_10mAh = 0xFFFF;
-    if (rem_10mAh > 0xFFFF) rem_10mAh = 0xFFFF;
+        if (full_10mAh > 0xFFFF) {
+      full_10mAh = 0xFFFF;
+    }
+    if (rem_10mAh > 0xFFFF) {
+      rem_10mAh = 0xFFFF;
+    }
 
     capacity_full_10mAh = (uint16_t)full_10mAh;
     capacity_remaining_10mAh = (uint16_t)rem_10mAh;
